3bb43e95cde0ff2dbcceb673659adc6bfe29b798
OmGhag <omghag2003@gmail.com>
2025-11-16 01:59:26 -0800

    task 4: exit

--- src/main.c
+++ src/main.c
@@ -2,15 +2,14 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define MAX_INPUT_SIZE 1024
 int main(int argc, char *argv[]) {
   // Flush after every printf
   setbuf(stdout, NULL);
   // Capture the user's command in the "command" variable
     printf("$ ");
-  const int max_size = 1024;
-  char inp[max_size];
-  while (fgets(inp, max_size, stdin) != NULL) {
-
+  char inp[MAX_INPUT_SIZE];
+  while ((fgets(inp, MAX_INPUT_SIZE, stdin) != NULL) || fgets(inp, MAX_INPUT_SIZE, stdin) == "exit") {
     // check if input is given
     inp[strlen(inp) - 1] = '\0';
     printf("%s: command not found\n", inp);
