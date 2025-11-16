945442ad2a668766502d5340629627ced4a339f5
OmGhag <omghag2003@gmail.com>
2025-11-16 02:55:20 -0800

    task 5: echo - fixed

--- src/main.c
+++ src/main.c
@@ -1,34 +1,73 @@
+
+72
+#include <stdbool.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-#define MAX_INPUT_SIZE 1024
-#define MAX_ARG_SIZE 64
-int main(int argc, char *argv[]) {
-    setbuf(stdout, NULL);  // Disable buffering for immediate output
-    
-    printf("$ ");
-    char inp[MAX_INPUT_SIZE];
-    char* args[MAX_ARG_SIZE];
+#define MAX_BUFFER_SIZE 128
 
-    while (fgets(inp, MAX_INPUT_SIZE, stdin) != NULL) {
-        // Remove trailing newline safely
-        size_t len = strlen(inp);
-        if (len > 0 && inp[len - 1] == '\n') {
-            inp[len - 1] = '\0';
-        }
+typedef struct {
+  char *input;
+  uint8_t input_size;
+  bool valid_input;
+} InputBuffer;
 
-        char* command = strtok(inp, " ");
-        if (strcmp(command, "echo") == 0){
-          printf("%s\n", inp + 5);
-          printf("$ ");
-        }else if (strncmp(inp, "exit", 4) == 0) {
-            return 0;   // exits with code 0
-        }else{
-          printf("%s: command not found\n", inp);
-          printf("$ ");
-        }
+InputBuffer Create_Input_Buffer() {
+  InputBuffer input_buffer;
+
+  input_buffer.input = calloc(MAX_BUFFER_SIZE, sizeof(char));
+  input_buffer.input_size = 0;
+  input_buffer.valid_input = false;
+
+  return input_buffer;
+}
+
+uint8_t capture_input(InputBuffer *input_buffer) {
+  fgets(input_buffer->input, MAX_BUFFER_SIZE, stdin);
+  input_buffer->input_size = strlen(input_buffer->input) - 1;
+  input_buffer->input[input_buffer->input_size] = '\0';
+
+  return input_buffer->input_size;
+}
+
+bool check_input(InputBuffer *input_buffer) {
+  char *command = strtok(input_buffer->input, " ");
+  if (strcmp(command, "echo") == 0) {
+    input_buffer->valid_input = true;
+    printf("%s\n", input_buffer->input + strlen(command) + 1);
+  } else if (strcmp(command, "exit") == 0) {
+    input_buffer->valid_input = true;
+    exit(EXIT_SUCCESS);
+  } else {
+    input_buffer->valid_input = false;
+  }
+
+  return input_buffer->valid_input;
+}
+
+void print_input_buffer(InputBuffer *input_buffer) {
+  printf("input: %s\n", input_buffer->input);
+  printf("input_size: %d\n", input_buffer->input_size);
+  printf("valid_input: %d\n", input_buffer->valid_input);
+}
+
+int main(void) {
+  setbuf(stdout, NULL);
+
+  printf("$ ");
+  InputBuffer input_buffer = Create_Input_Buffer();
+
+  while (capture_input(&input_buffer)) {
+    check_input(&input_buffer);
+    if (input_buffer.valid_input) {
+      ;
+    } else {
+      printf("%s: command not found\n", input_buffer.input);
     }
 
-    return 0;
+    printf("$ ");
+  }
+  return EXIT_SUCCESS;
 }
