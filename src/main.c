659f5ded6b503665dbb3a6ba37d0d9abc68634d8
OmGhag <omghag2003@gmail.com>
2025-11-16 01:34:04 -0800

    task 2: print invalid command

--- src/main.c
+++ src/main.c
@@ -4,9 +4,14 @@
 int main(int argc, char *argv[]) {
   // Flush after every printf
   setbuf(stdout, NULL);
+  //Capture the user's command in the "command" variable
+  char command[1024];
+   fgets(command, sizeof(command), stdin);
+   // Remove trailing newline character if present
+   command[strcspn(command, "\n")] = '\0';
 
-  // TODO: Uncomment the code below to pass the first stage
-   printf("$ ");
+   //Print the "<command>": "command not found" message
+   printf("command not found: %s\n", command);
 
   return 0;
 }
